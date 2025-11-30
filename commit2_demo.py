1a79de1249645a29d09330bad0ad0a773f8f5f30
Thushani01 <thupar03337@stud.noroff.no>
2025-11-30 21:09:19 +0530

    Add second Python file with two instances and ignore extra.txt

---
+++ commit2_demo.py
@@ -0,0 +1,16 @@
+# commit2_demo.py
+
+from user_input_validator import UserInputValidator
+
+def commit2_demo():
+    validator1 = UserInputValidator()
+    validator2 = UserInputValidator()
+
+    print("Commit 2 Output:")
+    print("Two UserInputValidator instances created.")
+    print("-----------------------------------")
+
+    return validator1, validator2
+
+if __name__ == "__main__":
+    commit2_demo()
